b8b824e56e2f63775d407cb71b23574c5ce0d0f8
Ola Mork <omork@mac.com>
2012-02-01 21:50:14 -0800

    fixed up .gitignore removed testing secret and added a random one to the gernator.

--- lib/generators/api_key_auth/api_key_auth_generator.rb
+++ lib/generators/api_key_auth/api_key_auth_generator.rb
@@ -7,5 +7,10 @@ class ApiKeyAuthGenerator < Rails::Generators::NamedBase
     copy_file "config/initializers/api-key-initializer.rb"
     migration_file_name = "#{Time.now.utc.strftime("%Y%m%d%H%M%S")}_create_api_keys.rb"
     copy_file "db/migrate/create_api_keys.rb", File.join(%w(db migrate), migration_file_name)
+
+    append_to_file "config/initializers/api-key-initializer.rb", %Q{
+# this can be changed to any random string of bytes it's used as the application's "private key"
+Rails.application.config.api_key_secret = "#{Digest::SHA256.hexdigest(Random.new.bytes(32))}"
+    }
   end
 end
--- lib/generators/api_key_auth/templates/config/initializers/api-key-initializer.rb
+++ lib/generators/api_key_auth/templates/config/initializers/api-key-initializer.rb
@@ -1,5 +1,4 @@
 Rails.application.config.api_key_owner_model = :user
-Rails.application.config.api_key_secret = %Q(3UOPrgkEskyIbWkijo8DZCs/kzmuG/8gmmQrAlr/sOq7PVviIzIRp0s5XTw0jdFPoio3QDHNOxJfvWZrJeb94Dp22nHPq4h8RrhHLC1tYfEav54JH/LCO5Gvyh7eEvAs)
 
 Rails.application.config.after_initialize do
   begin 
